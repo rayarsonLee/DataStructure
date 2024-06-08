0ed39348a42361a73efe5ca181f2bf58b0a491a5
lijiaxin.2333 <lijiaxin.2333@bytedance.com>
2024-06-08 12:43:54 +0800

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by lijiaxin on 2024/6/8.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
