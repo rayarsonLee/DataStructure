6dc6a88f3fdce5cc467792c9b1be314c861cf21b
lijiaxin.2333 <lijiaxin.2333@bytedance.com>
2024-06-08 13:47:47 +0800

    增加第一章绪论的大纲

--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -1,14 +1,27 @@
-//
-//  main.cpp
-//  DataStructure
-//
-//  Created by lijiaxin on 2024/6/8.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+using namespace std;
+
+// 第一章：绪论
+// 时间复杂度
+
+// O(1)
+// 考虑将输入的三个数按照从小到大的顺序重新排列
+// code:
+
+// O(logn)
+// 计算输入的整数的二进制中1的个数
+// code:
+
+// O(n)
+// 计算一维数组中n个数的和
+// code:
+
+// O(多项式)
+// 计算二维数组中n个数的和
+// code:
+
+// O(2^n)
+
+
+ 
